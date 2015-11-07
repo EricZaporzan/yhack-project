af345457f6b6179498d12ae2d579daf90859b90b
bong070 <bongkyu.song@mail.utoronto.ca>
2015-11-07 09:45:27 -0500

    updated version of GPSTracker

--- GetGPSLocation/src/com/example/getgpslocation/GPSTracker.java
+++ GetGPSLocation/src/com/example/getgpslocation/GPSTracker.java
@@ -1,13 +1,16 @@
 package com.example.getgpslocation;
 
-import java.security.Provider.Service;
-
+import android.app.Service;
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
 import android.os.IBinder;
+import android.provider.Settings;
 
 public class GPSTracker extends Service implements LocationListener{
 
@@ -15,15 +18,108 @@ public class GPSTracker extends Service implements LocationListener{
 	
 	boolean isGPSEnabled = false;
 	boolean canGetLocation = false;
+	boolean isNetworkEnabled = false;
+	
+	Location location;
 	
 	double lat;
 	double lon;
 	
 	private static final long MIN_DISTANCE_CHANGE_FOR_UPDATES = 10;
 	private static final long MIN_TIME_BW_UPDATES = 1000 * 60 * 1;
+
+	private static final String LOCATION_SERVICE = null;
 	
 	protected LocationManager locationManager;
+		
+	public GPSTracker(Context context) {
+		this.context = context;
+		getLocation();
+	}
 	
+	public Location getLocation() {
+		try {
+			locationManager = (LocationManager) context.getSystemService(LOCATION_SERVICE);
+			isGPSEnabled = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
+			isNetworkEnabled = locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+			if (!isGPSEnabled && !isNetworkEnabled) {
+				
+			} else {
+				this.canGetLocation = true;
+				if(isNetworkEnabled) {
+					locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, MIN_TIME_BW_UPDATES, MIN_DISTANCE_CHANGE_FOR_UPDATES, this);
+					if(locationManager != null) {
+						location = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+						if(location != null) {
+							lat = location.getLatitude();
+							lon = location.getLongitude();
+						}
+					}
+				}
+		    
+				if(isGPSEnabled) {
+					if(location == null) {
+						locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, MIN_TIME_BW_UPDATES, MIN_DISTANCE_CHANGE_FOR_UPDATES, this);
+						if(locationManager != null) {
+							location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+							lon = location.getLongitude();
+							lat = location.getLatitude();
+						}
+					}
+				}
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return location;
+	}
+	
+	public void stopUsingGPS() {
+		if(locationManager != null) {
+			locationManager.removeUpdates(GPSTracker.this);
+		}
+	}
+	
+	public double getLatitude(){
+		if(location != null) {
+			lat = location.getLatitude();
+		}
+		return lat;
+	}
+	
+	public double getLongitude() {
+		if(location != null) {
+			lon = location.getLongitude();
+		}
+		return lon;
+	}
+	
+	public boolean canGetLocation() {
+		return this.canGetLocation;
+	}
+	
+	public void showSettingAlert(){
+		AlertDialog.Builder alertDialog = new AlertDialog.Builder(context);
+		alertDialog.setTitle("GPS is settings");
+		alertDialog.setMessage("GPS is not enabled. Do you want to go to settings?");
+		alertDialog.setPositiveButton("Settings", new DialogInterface.OnClickListener() {
+						
+			@Override
+			public void onClick(DialogInterface dialog, int which) {
+				// TODO Auto-generated method stub
+				Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+				context.startActivity(intent);
+			}
+		});
+		alertDialog.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+			
+			@Override
+			public void onClick(DialogInterface dialog, int which) {
+				dialog.cancel();	
+			}
+		});
+		alertDialog.show();
+	}
 	@Override
 	public void onLocationChanged(Location location) {
 		// TODO Auto-generated method stub
@@ -46,6 +142,12 @@ public class GPSTracker extends Service implements LocationListener{
 	public void onProviderDisabled(String provider) {
 		// TODO Auto-generated method stub
 		
+	}
+
+	@Override
+	public IBinder onBind(Intent intent) {
+		// TODO Auto-generated method stub
+		return null;
 	}
 
 }
