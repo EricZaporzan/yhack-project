6306055545f99a4cfa3de98f0bc9ac2fecd3150f
bong070 <bongkyu.song@mail.utoronto.ca>
2015-11-07 09:44:57 -0500

    update version

--- GetGPSLocation/src/com/example/getgpslocation/MainActivity.java
+++ GetGPSLocation/src/com/example/getgpslocation/MainActivity.java
@@ -1,34 +1,40 @@
 package com.example.getgpslocation;
 
-import android.support.v7.app.ActionBarActivity;
+import android.app.AliasActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+//import android.view.Menu;
+//import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class MainActivity extends ActionBarActivity {
-
+public class MainActivity extends AliasActivity {
+	
+	Button btnShowLocation;
+	
+	GPSTracker gps;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		btnShowLocation = (Button)findViewById(R.id.show_location);
+		btnShowLocation.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				gps = new GPSTracker(MainActivity.this);
+				if(gps.canGetLocation()) {
+					double lat = gps.getLatitude();
+					double lon = gps.getLongitude();
+					Toast.makeText(getApplicationContext(), "Your Location is -\nLat: " + lat + "\nlong: " + lon, Toast.LENGTH_LONG).show();
+				} else {
+					gps.showSettingAlert();
+				}
+				
+			}
+		});
+		
 	}
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
-	}
-
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-		if (id == R.id.action_settings) {
-			return true;
-		}
-		return super.onOptionsItemSelected(item);
-	}
 }
