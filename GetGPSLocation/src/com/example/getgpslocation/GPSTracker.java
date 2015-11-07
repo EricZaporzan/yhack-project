ab8c98cca19e0eb55714ae38447ab625e8dea9cc
bong070 <bongkyu.song@mail.utoronto.ca>
2015-11-07 08:56:31 -0500

    GPS Tracker

---
+++ GetGPSLocation/src/com/example/getgpslocation/GPSTracker.java
@@ -0,0 +1,51 @@
+package com.example.getgpslocation;
+
+import java.security.Provider.Service;
+
+import android.content.Context;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.os.IBinder;
+
+public class GPSTracker extends Service implements LocationListener{
+
+	private final Context context;
+	
+	boolean isGPSEnabled = false;
+	boolean canGetLocation = false;
+	
+	double lat;
+	double lon;
+	
+	private static final long MIN_DISTANCE_CHANGE_FOR_UPDATES = 10;
+	private static final long MIN_TIME_BW_UPDATES = 1000 * 60 * 1;
+	
+	protected LocationManager locationManager;
+	
+	@Override
+	public void onLocationChanged(Location location) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onStatusChanged(String provider, int status, Bundle extras) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onProviderEnabled(String provider) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onProviderDisabled(String provider) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
